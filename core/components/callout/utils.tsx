2e9a0ae63f8fd39fe2484657c4e811fb04eeaee0
Felix Nana Yaw Yeboah-Jefferson <gabsco208309@gmail.com>
2025-02-21 17:23:33 +0000

    work for me

--- core/components/callout/utils.tsx
+++ core/components/callout/utils.tsx
@@ -1,12 +1,13 @@
-import React from "react"
-import { InfoIcon, AlertIcon } from "../icon/icon-list"
-import { CalloutVariant } from "./types"
+import React from 'react';
+
+import { AlertIcon, InfoIcon } from '../icon/icon-list';
+import { CalloutVariant } from './types';
 
 export const getVariantIcon = (variant: CalloutVariant): React.ReactNode => {
-  switch (variant) {
-    case "info":
-      return <InfoIcon size={5} />
-    case "danger":
-      return <AlertIcon size={5} />
-  }
-}
+    switch (variant) {
+        case 'info':
+            return <InfoIcon size="5" />;
+        case 'danger':
+            return <AlertIcon size="5" />;
+    }
+};
