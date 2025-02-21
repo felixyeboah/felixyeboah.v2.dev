ff72a72899ce676b18a29aa91d61267ed423695e
Felix Nana Yaw Yeboah-Jefferson <gabsco208309@gmail.com>
2025-02-21 17:25:52 +0000

    removed a module not found causing deployment errors

--- core/components/pill/types.ts
+++ core/components/pill/types.ts
@@ -1,7 +1,5 @@
-import { CSS } from "@/stitches.config"
-
 export interface PillProps extends React.HTMLAttributes<HTMLSpanElement> {
-  children: React.ReactNode;
-  variant: "info" | "danger" | "success" | "warning";
-  css?: CSS;
+    children: React.ReactNode;
+    variant: 'info' | 'danger' | 'success' | 'warning';
+    css?: React.CSSProperties;
 }
