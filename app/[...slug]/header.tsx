022ffbd76c190872690576217b1617a9f6d8fca3
Felix Nana Yaw Yeboah <87368189+felixyeboah@users.noreply.github.com>
2025-04-07 01:18:35 +0000

    Add GSAP animations to header component for enhanced text transitions
    Implemented character and line animations using GSAP and SplitType in the Header component. The header now features staggered entrance effects for both the title and paragraph text, improving visual appeal and user engagement.

--- app/[...slug]/header.tsx
+++ app/[...slug]/header.tsx
@@ -2,16 +2,71 @@
 
 import { FrontMatterPost } from '@/types/post';
 import Image from 'next/image';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 
 import { cn } from '../libs/misc';
 import { loader } from '../libs/next-image-loader';
+import { useGSAP } from '@gsap/react';
+import SplitType from 'split-type';
+import gsap from 'gsap';
 
 export const Header = ({ page }: { page: FrontMatterPost }) => {
     const [isLoading, setLoading] = useState(true);
 
+    const container = useRef<HTMLDivElement>(null);
+
+    useGSAP(
+        () => {
+            const heroText = new SplitType('.header-text h1', {
+                types: 'chars',
+            });
+            const text = new SplitType('.header-text p', {
+                types: 'lines',
+                tagName: 'div',
+                lineClass: 'line',
+            });
+            gsap.set(heroText.chars, {
+                y: 400,
+            });
+
+            gsap.to(heroText.chars, {
+                y: 0,
+                duration: 1,
+                stagger: 0.075,
+                ease: 'power4.out',
+                delay: 1,
+            });
+
+            // paragraph lines
+            text.lines?.forEach((line) => {
+                const content = line.innerHTML;
+                line.innerHTML = `<span>${content}</span>`;
+            });
+
+            gsap.set('.header-text p .line span', {
+                y: 400,
+                display: 'block',
+            });
+
+            gsap.to('.header-text p .line span', {
+                y: 0,
+                duration: 2,
+                stagger: 0.075,
+                ease: 'power4.out',
+                delay: 0.25,
+            });
+
+            return () => {
+                if (text) text.revert();
+            };
+        },
+        {
+            scope: container,
+        },
+    );
+
     return (
-        <header className="flex flex-col gap-10 items-center justify-center lg:pt-40">
+        <header className="header-text flex flex-col gap-10 items-center justify-center lg:pt-40" ref={container}>
             <h1 className="block text-5xl md:text-8xl font-medium text-white">
                 {page?.frontMatter.title}
             </h1>
